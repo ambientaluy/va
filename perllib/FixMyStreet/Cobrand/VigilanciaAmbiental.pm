14122d8d0751041beb309540b63b0d0c617aa26f
daniloide <daniloide@gmail.com>
2017-10-25 15:33:22 -0300

    Cobrand VigilanciaAmbiental (perl module)

---
+++ perllib/FixMyStreet/Cobrand/VigilanciaAmbiental.pm
@@ -0,0 +1,16 @@
+package FixMyStreet::Cobrand::VigilanciaAmbiental;
+use base 'FixMyStreet::Cobrand::PorMiBarrio';
+
+use strict;
+use warnings;
+use JSON;
+use HTTP::Request::Common;
+use Data::Dumper;
+use LWP::UserAgent;
+use DateTime;
+use Switch;
+use URI;
+#use Params::Util qw<_HASH _HASH0 _HASHLIKE>;
+
+
+1;
